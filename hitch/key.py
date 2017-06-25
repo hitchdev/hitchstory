f76ae2d0fc96eebf50942a3b308d5d85dc9b5f40
Colm O'Connor <colm.oconnor.github@gmail.com>
2017-06-25 14:22:52 +0100

    FEATURE : Added __version__ variable to __init__.py, updated on deployment.

--- hitch/key.py
+++ hitch/key.py
@@ -336,10 +336,12 @@ def deploy(version):
     """
     Deploy to pypi as specified version.
     """
-    version_file = KEYPATH.parent.joinpath("VERSION")
+    NAME = "hitchstory"
+    git = Command("git").in_dir(DIR.project)
+    version_file = DIR.project.joinpath("VERSION")
     old_version = version_file.bytes().decode('utf8')
     if version_file.bytes().decode("utf8") != version:
-        KEYPATH.parent.joinpath("VERSION").write_text(version)
+        DIR.project.joinpath("VERSION").write_text(version)
         git("add", "VERSION").run()
         git("commit", "-m", "RELEASE: Version {0} -> {1}".format(
             old_version,
@@ -350,7 +352,21 @@ def deploy(version):
         git("push", "origin", version).run()
     else:
         git("push").run()
-    python("setup.py", "sdist", "upload").in_dir(KEYPATH.parent).run()
+
+    # Set __version__ variable in __init__.py, build sdist and put it back
+    initpy = DIR.project.joinpath(NAME, "__init__.py")
+    original_initpy_contents = initpy.bytes().decode('utf8')
+    initpy.write_text(
+        original_initpy_contents.replace("DEVELOPMENT_VERSION", version)
+    )
+    python("setup.py", "sdist").in_dir(DIR.project).run()
+    initpy.write_text(original_initpy_contents)
+
+    # Upload to pypi
+    python(
+        "-m", "twine", "upload", "dist/{0}-{1}.tar.gz".format(NAME, version)
+    ).in_dir(DIR.project).run()
+
 
 
 def docgen():
--- hitchstory/__init__.py
+++ hitchstory/__init__.py
@@ -2,3 +2,5 @@ from hitchstory.story_collection import StoryCollection
 from hitchstory.engine import validate
 from hitchstory.engine import BaseEngine
 from hitchstory.engine import StorySchema
+
+__version__ = "DEVELOPMENT_VERSION"
