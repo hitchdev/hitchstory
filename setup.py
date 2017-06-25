fdba2d10579a5906e581f945f5c24c1be512cfca
Colm O'Connor <colm.oconnor.github@gmail.com>
2017-06-25 14:20:39 +0100

    DOCS : Updated project description.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*parts):
 
 setup(name="hitchstory",
     version=read('VERSION').replace('\n', ''),
-    description="DSL parser designed to execute .story files.",
+    description="BDD, strongly typed with StrictYAML and Python.",
     long_description=read('README.rst'),
     classifiers=[
         'Development Status :: 3 - Alpha',
