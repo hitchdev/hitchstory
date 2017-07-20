7758a661587e219d4630d9a73b2e3d86a2501581
Colm O'Connor <colm.oconnor.github@gmail.com>
2017-07-20 22:28:25 +0100

    BUG : HitchStoryException was not importable from the root of the module.

--- hitchstory/__init__.py
+++ hitchstory/__init__.py
@@ -2,5 +2,6 @@ from hitchstory.story_collection import StoryCollection
 from hitchstory.engine import validate
 from hitchstory.engine import BaseEngine
 from hitchstory.engine import StorySchema
+from hitchstory.exceptions import HitchStoryException
 
 __version__ = "DEVELOPMENT_VERSION"
