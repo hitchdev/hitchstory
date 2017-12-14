065c78e89143e1131d8c9d188d882a60ce52738c
Colm O'Connor <colm.oconnor.github@gmail.com>
2017-12-14 08:16:15 +0000

    REFACTOR : Refactored key.py

--- hitch/key.py
+++ hitch/key.py
@@ -244,7 +244,7 @@ def tdd(*words):
             Engine(
                 DIR,
                 {
-                    "overwrite artefacts": True,
+                    "overwrite artefacts": False,
                     "print output": True,
                 },
             )
