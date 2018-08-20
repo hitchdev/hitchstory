47b31d52a4effac3262307cc950aee3ec2bc40fc
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-08-20 16:33:47 +0100

    MISC : Added doc generation.

--- hitch/key.py
+++ hitch/key.py
@@ -381,3 +381,39 @@ def rerun(version="3.5.0"):
     Command(DIR.gen.joinpath("py{0}".format(version), "bin", "python"))(
         DIR.gen.joinpath("state", "examplepythoncode.py")
     ).in_dir(DIR.gen.joinpath("state")).run()
+
+
+@expected(dirtemplate.exceptions.DirTemplateException)
+def docgen():
+    """
+    Build documentation.
+    """
+    def title(dirfile):
+        assert len(dirfile.text().split("---")) >= 3, "{} doesn't have ---".format(dirfile)
+        return load(dirfile.text().split("---")[1]).data.get("title", "misc")
+
+    docfolder = DIR.gen/"docs"
+
+    if docfolder.exists():
+        docfolder.rmtree(ignore_errors=True)
+    docfolder.mkdir()
+
+    template = dirtemplate.DirTemplate(
+        "docs", DIR.project/"docs", DIR.gen,
+    ).with_files(
+        template_story_jinja2={
+            "using/alpha/{0}.md".format(story.info['docs']): {"story": story}
+            for story in _storybook({}).ordered_by_name()
+            if story.info.get("docs") is not None
+        },
+    ).with_vars(
+        readme=False,
+        quickstart=_storybook({})
+        .in_filename(DIR.key/"quickstart.story")
+        .non_variations()
+        .ordered_by_file(),
+    ).with_functions(
+        title=title
+    )
+    template.ensure_built()
+    print("Docs generated")
