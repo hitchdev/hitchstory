f9cd539f8752c6a7c8e5f2f6e2c2c619cb6d7da2
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-01-26 11:16:28 +0000

    REFACTOR : Improve new style doc generation.

--- hitch/docgen.py
+++ hitch/docgen.py
@@ -1,10 +1,13 @@
 def run_docgen(paths, storybook):
-    storydocs = storybook.with_documentation(paths.key.joinpath("docstory.yml").text())
+    storydocs = storybook.with_documentation(
+        paths.key.joinpath("docstory.yml").text(),
+        extra={"in_interpreter": False, "include_title": True},
+    )
 
-    for story in storydocs:
+    for story in storydocs.ordered_by_file():
         docfilename = story.info.get("docs")
         if docfilename is not None:
-            docpath = paths.joinpath(
-                "docs", "public", "using", "alpha", docfilename + ".md"
+            docpath = paths.project.joinpath(
+                "docs", "public", "using", docfilename + ".md"
             )
-            # docpath.write_text(story.documentation())
+            docpath.write_text(story.documentation())
