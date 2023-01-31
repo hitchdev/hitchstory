017a5dc141f1197c621bc8219b43bbaf77681987
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-01-31 11:21:00 +0000

    TOOLING : New style docgen.

--- hitch/docgen.py
+++ hitch/docgen.py
@@ -1,4 +1,5 @@
 from strictyaml import load
+from commandlib import python_bin
 
 
 README_INTRO = """# HitchStory
@@ -34,28 +35,46 @@ def _contents(main_folder, folder):
 
 
 def run_docgen(paths, storybook):
+    dirtempl = python_bin.dirtempl.in_dir(paths.project / "docs")
     doc_src = paths.project / "docs" / "src"
+    dest_path = paths.project / "docs" / "draft"
     snippets_path = paths.project / "docs" / "snippets"
     
     if snippets_path.exists():
         snippets_path.rmtree()
     snippets_path.mkdir()
     
+    if dest_path.exists():
+        dest_path.rmtree()
+    
+    generate_storydocs(
+        paths.key.joinpath("docstory.yml").text(),
+        doc_src.joinpath("using"),
+        storybook,
+    )
+    
     snippets_path.joinpath("intro.txt").write_text(DOCS_INTRO)
     snippets_path.joinpath("why-contents.txt").write_text(
         _contents(doc_src, "why")
     )
+    snippets_path.joinpath("why-not-contents.txt").write_text(
+        _contents(doc_src, "why-not")
+    )
+    snippets_path.joinpath("approach-contents.txt").write_text(
+        _contents(doc_src, "approach")
+    )
+    snippets_path.joinpath("using-contents.txt").write_text(
+        _contents(doc_src, "using")
+    )
+    dirtempl("--snippets", "snippets", "src", dest_path).run()
 
-def generate_storydocs(paths, storybook):
+def generate_storydocs(docstory, docpath, storybook):
     storydocs = storybook.with_documentation(
-        paths.key.joinpath("docstory.yml").text(),
+        docstory,
         extra={"in_interpreter": False, "include_title": True},
     )
 
     for story in storydocs.ordered_by_file():
         docfilename = story.info.get("docs")
         if docfilename is not None:
-            docpath = paths.project.joinpath(
-                "docs", "public", "using", docfilename + ".md"
-            )
-            docpath.write_text(story.documentation())
+            docpath.joinpath(docfilename + ".md").write_text(story.documentation())
