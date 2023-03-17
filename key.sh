97b9e50d9b51d56f5c65a5a91548c9bfa5d6dcd6
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-03-17 16:26:05 +0000

    TOOLING : Updated key.sh.

--- key.sh
+++ key.sh
@@ -1,42 +1,82 @@
 #!/bin/bash
 set -e
+PROJECT_NAME=hitchstory
+
 PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+FOLDER_HASH=$(echo $PROJECT_DIR | md5sum | cut -c 1-5)
+GEN_VOLUME_NAME=${PROJECT_NAME}-hitch-gen
+IMAGE_NAME=${PROJECT_NAME}-hitch
 
 hitchrun() {
     podman run --privileged -it --rm \
         -v $PROJECT_DIR:/src \
-        -v hitchstory-hitch-container:/gen \
+        -v $GEN_VOLUME_NAME:/gen \
         -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
         -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
         -p 5555:5555 \
         --secret pypitoken,type=env,target=PYPITOKEN \
         --workdir /src \
-        hitchstory-hitch \
+        $IMAGE_NAME \
         $1
 }
 
 
 case "$1" in
     "clean")
-        if podman volume exists hitchstory-hitch-container; then
-            podman volume rm hitchstory-hitch-container
-        fi
-        if podman image exists hitchstory-hitch; then
-            podman image rm -f hitchstory-hitch
-        fi
+        case "$2" in
+            "all")
+                if podman image exists $IMAGE_NAME; then
+                    podman image rm -f $IMAGE_NAME
+                fi
+                if podman volume exists $GEN_VOLUME_NAME; then
+                    podman volume rm -f $GEN_VOLUME_NAME
+                fi
+                ;;
+            "gen")
+                hitchrun "rm -rf /gen/*"
+                ;;
+            *)
+                echo "Invalid clean target. ./key.sh clean [all]"
+                exit 1
+                ;;
+        esac
         ;;
     "make")
-        echo "building ci container..."
-        if ! podman volume exists hitchstory-hitch-container; then
-            podman volume create hitchstory-hitch-container
-        fi
-        podman build -f hitch/Dockerfile-hitch -t hitchstory-hitch $PROJECT_DIR
+        case "$2" in
+            "")
+                echo "building ci container..."
+                if ! podman volume exists $GEN_VOLUME_NAME; then
+                    podman volume create $GEN_VOLUME_NAME
+                fi
+                podman build -f hitch/Dockerfile-hitch -t $IMAGE_NAME $PROJECT_DIR
+                hitchrun "virtualenv --python=python3 /gen/venv"
+                hitchrun "/gen/venv/bin/pip install setuptools-rust"
+                hitchrun "/gen/venv/bin/pip install -r /src/hitch/hitchreqs.txt"
+                hitchrun "/gen/venv/bin/python hitch/key.py build"
+                ;;
+            "gen")
+                hitchrun "virtualenv --python=python3 /gen/venv"
+                hitchrun "/gen/venv/bin/pip install setuptools-rust"
+                hitchrun "/gen/venv/bin/pip install -r /src/hitch/hitchreqs.txt"
+                hitchrun "/gen/venv/bin/python hitch/key.py build"
+                ;;
+            "pylibrarytoolkit")
+                hitchrun "virtualenv --python=python3 /gen/venv"
+                hitchrun "/gen/venv/bin/pip install --upgrade pip setuptools wheel setuptools-rust"
+                hitchrun "/gen/venv/bin/pip install -e /src/hitchpylibrarytoolkit"
+                hitchrun "/gen/venv/bin/python hitch/key.py build"
+                ;;
+            *)
+                echo "Invalid make target. ./key.sh make [all|gen|pylibrarytoolkit]"
+                exit 1
+                ;;
+            esac
         ;;
     "bash")
         hitchrun "bash"
         ;;
     *)
-        hitchrun "/venv/bin/python hitch/key.py $1 $2 $3 $4 $5 $6 $7 $8 $9"
+        hitchrun "/gen/venv/bin/python hitch/key.py $1 $2 $3 $4 $5 $6 $7 $8 $9"
         ;; 
 esac
 
