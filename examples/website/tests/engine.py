3f00ebc09f6ae24c844be594fd82186b6094b7d7
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-05-05 18:55:46 +0100

    EXAMPLES : Cleaned up engine.py

--- examples/website/tests/engine.py
+++ examples/website/tests/engine.py
@@ -1,98 +1,18 @@
-from hitchstory import (
-    StoryCollection,
-    BaseEngine,
-    exceptions,
-    validate,
-    no_stacktrace_for,
-)
+from hitchstory import StoryCollection, BaseEngine
+from hitchstory import exceptions, validate
 from hitchstory import GivenDefinition, GivenProperty, InfoDefinition, InfoProperty
 from strictyaml import Optional, Str, Map, Int, Bool, Enum, load, MapPattern
-from path import Path
-from shlex import split
+from playwright.sync_api import expect
 from commandlib import Command, CommandError
-import requests
-import time
-from playwright.sync_api import sync_playwright, expect
-from slugify import slugify
+from podman import PlaywrightServer, App
 from video import convert_to_slow_gif
+from slugify import slugify
 from pathlib import Path
-
-# This lets IPython.embed play well with playwright
-# since they both run an event loop
 import nest_asyncio
 
 nest_asyncio.apply()
-# Use __import__('IPython').embed()
-
 
 PROJECT_DIR = Path(__file__).absolute().parents[0].parent
-
-
-class App:
-    """Run and interact with the web server."""
-
-    def __init__(self, podman):
-        self._podman = podman
-
-    def start(self):
-        self._podman(
-            "run", "--rm", "-v", "/src/app:/app", "-d", "--name", "app", "app"
-        ).output()
-
-    def wait_until_ready(self):
-        """Wait for port message"""
-        logs = self._podman("logs", "-f", "app").interact().run()
-        logs.wait_until_output_contains("Running on http://127.0.0.1:5000")
-        logs.kill()
-
-    def stop(self):
-        self._podman("stop", "app", "--time", "1").ignore_errors().output()
-
-    def logs(self):
-        try:
-            self._podman("logs", "app").run()
-        except CommandError:
-            pass
-
-
-class PlaywrightServer:
-    """
-    Runs the server, grab a new page.
-    """
-
-    def __init__(self, podman):
-        self._podman = podman
-        self._ws = None
-
-    def start(self):
-        self._podman("run", "--rm", "-d", "--name", "playwright", "playwright").output()
-
-    def wait_until_ready(self):
-        """Wait for logs to print out port."""
-        logs = self._podman("logs", "-f", "playwright").interact().run()
-        logs.wait_until_output_contains("Listening on")
-        self._ws = logs.stripshot().replace("Listening on", "").strip()
-        logs.kill()
-
-    def stop(self):
-        if hasattr(self, "_browser"):
-            self._browser.close()
-        if hasattr(self, "_playwright"):
-            self._playwright.stop()
-        try:
-            self._podman("stop", "playwright", "--time", "1").ignore_errors().output()
-        except CommandError:
-            pass
-
-    def new_page(self):
-        self._playwright = sync_playwright().start()
-        self._browser = self._playwright.chromium.connect(self._ws).new_context(
-            record_video_dir="videos/"
-        )
-        self._page = self._browser.new_page()
-        self._page.set_default_navigation_timeout(10000)
-        self._page.set_default_timeout(10000)
-        return self._page
 
 
 class Engine(BaseEngine):
@@ -161,16 +81,12 @@ class Engine(BaseEngine):
     def on_failure(self, result):
         """Run before teardown, only on failure."""
         if hasattr(self, "_page"):
-            self._page.screenshot(
-                path=PROJECT_DIR / "docs" / "failure.png"
-            )
+            self._page.screenshot(path=PROJECT_DIR / "docs" / "failure.png")
             PROJECT_DIR.joinpath("docs", "failure.html").write_text(
                 self._page.content()
             )
             self._page.close()
-            self._page.video.save_as(
-                PROJECT_DIR / "docs" / "failure.webm"
-            )
+            self._page.video.save_as(PROJECT_DIR / "docs" / "failure.webm")
         if hasattr(self, "_app"):
             self._app.logs()
 
---
+++ examples/website/tests/podman.py
@@ -0,0 +1,69 @@
+from commandlib import CommandError
+from playwright.sync_api import sync_playwright
+
+
+class App:
+    """Run and interact with the web server."""
+
+    def __init__(self, podman):
+        self._podman = podman
+
+    def start(self):
+        self._podman(
+            "run", "--rm", "-v", "/src/app:/app", "-d", "--name", "app", "app"
+        ).output()
+
+    def wait_until_ready(self):
+        """Wait for port message"""
+        logs = self._podman("logs", "-f", "app").interact().run()
+        logs.wait_until_output_contains("Running on http://127.0.0.1:5000")
+        logs.kill()
+
+    def stop(self):
+        self._podman("stop", "app", "--time", "1").ignore_errors().output()
+
+    def logs(self):
+        try:
+            self._podman("logs", "app").run()
+        except CommandError:
+            pass
+
+
+class PlaywrightServer:
+    """
+    Runs the server, grab a new page.
+    """
+
+    def __init__(self, podman):
+        self._podman = podman
+        self._ws = None
+
+    def start(self):
+        self._podman("run", "--rm", "-d", "--name", "playwright", "playwright").output()
+
+    def wait_until_ready(self):
+        """Wait for logs to print out port."""
+        logs = self._podman("logs", "-f", "playwright").interact().run()
+        logs.wait_until_output_contains("Listening on")
+        self._ws = logs.stripshot().replace("Listening on", "").strip()
+        logs.kill()
+
+    def stop(self):
+        if hasattr(self, "_browser"):
+            self._browser.close()
+        if hasattr(self, "_playwright"):
+            self._playwright.stop()
+        try:
+            self._podman("stop", "playwright", "--time", "1").ignore_errors().output()
+        except CommandError:
+            pass
+
+    def new_page(self):
+        self._playwright = sync_playwright().start()
+        self._browser = self._playwright.chromium.connect(self._ws).new_context(
+            record_video_dir="videos/"
+        )
+        self._page = self._browser.new_page()
+        self._page.set_default_navigation_timeout(10000)
+        self._page.set_default_timeout(10000)
+        return self._page
