8e87561cabde2221a4a5cbb2eab82020c7d42910
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-05-10 12:35:39 +0100

    EXAMPLES : Website project - updated gifs.

--- examples/website/tests/video.py
+++ examples/website/tests/video.py
@@ -12,13 +12,19 @@ def convert_to_slow_gif(webm_path: Path):
     play anything else on loop.
     """
     gif_path = webm_path.parent / f"{webm_path.stem}.gif"
+    webm_temp = webm_path.parent / "webm_temp.webm"
     palette_path = webm_path.parent / "palette.png"
 
     ffmpeg = Command("ffmpeg", "-y")
-    ffmpeg("-i", webm_path, "-vf", "palettegen", palette_path).output()
+    
+    # Cut first second - just blank loading
+    ffmpeg("-i", webm_path, "-ss", "1", "-t", "1", "-fflags", "+genpts", webm_temp).output()
+
+    # Convert to GIF
+    ffmpeg("-i", webm_temp, "-vf", "palettegen", palette_path).output()
     ffmpeg(
         "-i",
-        webm_path,
+        webm_temp,
         "-i",
         palette_path,
         "-filter_complex",
@@ -27,6 +33,11 @@ def convert_to_slow_gif(webm_path: Path):
         "10",
         gif_path,
     ).output()
-    Command("convert", "-delay", "40x100", gif_path, gif_path).run()
+    
+    # Slow down GIF
+    Command("convert", "-delay", "50x100", gif_path, gif_path).run()
+    
+    # Clean up
     webm_path.unlink()
+    webm_temp.unlink()
     palette_path.unlink()
