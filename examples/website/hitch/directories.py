032dcf1cb88d8be8069b1ef14d6789642f45feae
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-10-31 00:51:30 +0000

    EXAMPLES : Moved tests into hitch dir.

--- examples/website/hitch/directories.py
+++ examples/website/hitch/directories.py
@@ -6,9 +6,9 @@ ROOT = Path(__file__).absolute().parents[0].parent
 class DIR:
     PROJECT = ROOT
     APP = ROOT / "app"
-    ARTEFACTS = ROOT / "artefacts"
-    DOCS = ROOT / "docs"
-    STORY = ROOT / "story"
+    ARTEFACTS = ROOT / "hitch" / "artefacts"
+    DOCS = ROOT / "hitch" / "docs"
+    STORY = ROOT / "hitch" / "story"
     HITCH = ROOT / "hitch"
     SELECTORS = ROOT / "hitch" / "selectors"
     DATACACHE = Path("/gen")
